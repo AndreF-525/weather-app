a2fc8e4c23b775b9479a0c8ba5ff63932e975ac5
André Montaño <andrefernando2000@gmail.com>
2025-08-04 21:46:08 -0400

    Modulos css, types, data y formulario

---
+++ src/components/Form/Form.tsx
@@ -0,0 +1,53 @@
+import { useState, type ChangeEvent } from "react"
+import type { SearchType } from "../../types"
+import { countries } from "../../data/countries"
+import styles from './Form.module.css'
+
+export default function Form() {
+
+  const [search, setSearch] = useState<SearchType>({
+    city: '',
+    country: ''
+  })
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement> | ChangeEvent<HTMLSelectElement>) => { 
+    setSearch({
+      ...search,
+      [e.target.name] : e.target.value
+    })
+  }
+
+  return (
+    <form className={styles.form}>
+
+      <div className={styles.field}>
+        <label htmlFor="city">Ciudad</label>
+        <input
+          type="text"
+          id="city"
+          name="city"
+          placeholder="Ciudad"
+          value={search.city}
+          onChange={handleChange} />
+      </div>
+
+      <div className={styles.field}>
+        <label htmlFor="country">País</label>
+        <select
+          id="country"
+          name="country"
+          onChange={handleChange}
+          value={search.country}>
+          <option>--Seleccione un país--</option>
+          {countries.map(country => (
+            <option key={country.code} value={country.code}>
+              {country.name}
+            </option>
+          ))}
+        </select>
+      </div>
+
+      <input className={styles.submit} type="submit" value="Consultar Clima" />
+    </form>
+  )
+}
